760d920d71a384cde1022ed76fa7e410dfac4ea5
DanSjukdom <daniel.py.216@gmail.com>
2020-09-06 18:20:08 -0500

    Erased the sample http request

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -7,9 +7,3 @@ urlpatterns = [
     path('', views.PostsListsAPI.as_view(), name='posts-list'),
     path('create/', views.PostCreateAPI.as_view(), name='post-create'),
 ]
-
-
-
-"""
-http -f post http://localhost:8000/api/accounts/register/ username="Kratos" password="hello world" confirm_password="hello world"
-"""
