09dc8bb96a0bac62828ea4287c8e780b96853771
DanSjukdom <daniel.py.216@gmail.com>
2020-09-06 19:16:06 -0500

    Create and detail views

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,18 +18,48 @@ class PostsListsAPI(APIView):
         )
 
 
-class PostCreateAPI(APIView):
+class PostsCreateAPI(APIView):
     def post(self, request):
-        print(request.data)
         serializer = PostSerializer(data=request.data)
-        print(serializer.is_valid())
         if serializer.is_valid():
-            serializer.save(author=request.user)
+            post = serializer.save(author=request.user)
             return Response(
-                serializer.data,
+                PostSerializer(post).data,
                 status=status.HTTP_201_CREATED
             )
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class PostsDetailAPI(APIView):
+    def get(self, request, id):
+        post = get_object_or_404(Post, id=id)
+        serializer = PostSerializer(post)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_200_OK
+        )
+    def put(self, request, id):
+        post = get_object_or_404(Post, id=id)
+        serializer = PostSerializer(instance=post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                PostSerializer(post).data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    def delete(self, request, id):
+        post = get_object_or_404(Post, id=id)
+        post.delete
+        return Response(
+            {
+                "message": "Post deleted successfully"
+            }, 
+            status=status.HTTP_200_OK
         )
