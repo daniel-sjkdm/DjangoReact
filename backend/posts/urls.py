7dcf0984f07fbd4586b279e639c00038ce46751f
DanSjukdom <daniel.py.216@gmail.com>
2020-09-06 19:16:50 -0500

    API entrypoints

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.PostsListsAPI.as_view(), name='posts-list'),
-    path('create/', views.PostCreateAPI.as_view(), name='post-create'),
+    path('create/', views.PostsCreateAPI.as_view(), name='post-create'),
+    path('<int:id>/', views.PostsDetailAPI.as_view(), name='post-detail')
 ]
