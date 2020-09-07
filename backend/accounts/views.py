f214757ad3a25e6c9d3dc5d0e95191bae530b13b
DanSjukdom <daniel.py.216@gmail.com>
2020-09-06 19:29:21 -0500

    Login api added

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
 from django.shortcuts import render
-from .serializers import AccountSerializer, AccountLoginSerialiazer, AccountRegisterSerializer
+from .serializers import AccountSerializer, AccountLoginSerializer, AccountRegisterSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -35,7 +36,27 @@ class AccountRegisterAPI(APIView):
 
 
 class AccountLoginAPI(APIView):
-    pass
+    permission_classes = []
+    authentication_classes = []
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            serializer = AccountLoginSerializer(user)
+            return Response(
+                {
+                    "username": serializer.data,
+                    "token": "some token here"
+                },
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            {
+                "error": "The username/password is invalid"
+            },
+            status=status.HTTP_403_FORBIDDEN
+        )
 
 
 class AccountLogoutAPI(APIView):
