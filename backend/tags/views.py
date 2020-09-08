c8d81b1952a08c01a6b0ef4f18aa41b18b8f90b9
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-08 13:57:24 -0500

    CRUD views

--- backend/tags/views.py
+++ backend/tags/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Tag
 from rest_framework.views import APIView
 from rest_framework import status
@@ -15,3 +15,53 @@ class TagListAPI(APIView):
             serializer.data, 
             status=status.HTTP_200_OK
         )
+
+
+class TagAPI(APIView):
+    def get(self, request, id):
+        tag = get_object_or_404(Tag, id=id)
+        serializer = TagSerializer(tag)
+        return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+        )
+
+
+class TagUpdateAPI(APIView):
+    def put(self, request, id):
+        tag = get_object_or_404(Tag, id=id)
+        serializer = TagSerializer(instance=tag, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                    serializer.data, 
+                    status=status.HTTP_200_OK
+            )
+
+
+class TagDeleteAPI(APIView):
+    def delete(self, request, id):
+        tag = get_object_or_404(Tag, id=id)
+        tag.delete()
+        return Response(
+                {
+                    "message": "Tag deleted successfully"
+                },
+                status = status.HTTP_200_OK
+        )
+
+
+class TagCreateAPI(APIView):
+    def post(self, request):
+        serializer = TagSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                    serializer.data,
+                    status=status.HTTP_201_CREATED
+            )
+        return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+        )
+
