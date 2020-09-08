c321c2cf96e2061b26aca1517bcc797bb579fb31
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-08 13:57:07 -0500

    CRUD urls

--- backend/tags/urls.py
+++ backend/tags/urls.py
@@ -6,4 +6,8 @@ from . import views
 app_name = 'tags'
 urlpatterns = [
     path('', views.TagListAPI.as_view(), name='tags-list'),
+    path('<int:id>/', views.TagAPI.as_view(), name='tag-detail'),
+    path('<int:id>/update/', views.TagUpdateAPI.as_view(), name='tag-update'),
+    path('<int:id>/delete/', views.TagDeleteAPI.as_view(), name='tag-delete'),
+    path('create/', views.TagCreateAPI.as_view(), name='tag-create')
 ]
