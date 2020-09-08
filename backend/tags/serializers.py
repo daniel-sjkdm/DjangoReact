aa4e8406c027c3755db14d646ba8a0285699a44d
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-08 13:56:46 -0500

    CRUD serializers

--- backend/tags/serializers.py
+++ backend/tags/serializers.py
@@ -5,4 +5,7 @@ from .models import Tag
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ['title']
+        fields = [
+                'id',
+                'title'
+        ]
