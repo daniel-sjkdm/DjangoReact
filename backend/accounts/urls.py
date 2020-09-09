4599cadfa61cb44b7d5c81ae6393adc66fc604d6
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-09 12:04:34 -0500

    Erase logout

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', views.AccountAPI.as_view(), name='accounts'),
     path('register/', views.AccountRegisterAPI.as_view(), name='register'),
     path('login/', views.AccountLoginAPI.as_view(), name='login'),
-    # path('logout/', views.AccountLogoutAPI.as_view(), name='logout'),
 ]
