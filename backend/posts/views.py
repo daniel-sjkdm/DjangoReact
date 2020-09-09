612a46e703b897f55d53e29426aaf596c41b6f27
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-09 17:14:50 -0500

    Like button functionality

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -81,6 +81,21 @@ class PostsDetailAPI(APIView):
         )
 
 
+class PostLikeAPI(APIView):
+    def post(self, request, id):
+        post = get_object_or_404(Post, id=id)
+        if post.liked_by.filter(username__iexact=request.user):
+            post.liked_by.remove(request.user)
+        else:
+            post.liked_by.add(request.user)   
+        post.save()
+        serializer = PostSerializer(post)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+
         # http -f --auth-type=jwt  --auth="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJ1c2VybmFtZSI6IlR5bGVyIiwiZXhwIjoxNTk5NjcxODExLCJlbWFpbCI6IiIsIm9yaWdfaWF0IjoxNTk5NjcxNTExfQ.b9QDFVQVE2oN1wFzeNVMZs-2ADhqZHOS7MZspDxekYk" post http://127.0.0.1:8000/api/posts/create/  title="test jwt" content="test jwt" tags="jwt, django"
 
 
