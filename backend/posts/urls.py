1fadf7f7a239e6f4db84ea9009259c0651683900
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-09 17:23:57 -0500

    Post like path api

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -6,5 +6,6 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.PostsListsAPI.as_view(), name='posts-list'),
     path('create/', views.PostsCreateAPI.as_view(), name='post-create'),
-    path('<int:id>/', views.PostsDetailAPI.as_view(), name='post-detail')
+    path('<int:id>/', views.PostsDetailAPI.as_view(), name='post-detail'),
+    path('<int:id>/like/', views.PostLikeAPI.as_view(), name='post-like')
 ]
