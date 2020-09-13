a1d7a464a05d3d68dc9741fefc4a6b07ec44e6cb
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-13 17:11:41 -0500

    Search bar api view

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.PostsListsAPI.as_view(), name='posts-list'),
     path('create/', views.PostsCreateAPI.as_view(), name='post-create'),
     path('<int:id>/', views.PostsDetailAPI.as_view(), name='post-detail'),
+    path('query/', views.PostsQueryAPI.as_view(), name='post-query'),
     path('<int:id>/like/', views.PostLikeAPI.as_view(), name='post-like')
 ]
