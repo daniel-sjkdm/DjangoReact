e93f9c6a09a584f7bf88090126a63c6bc4100c74
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-13 17:05:02 -0500

    Search bar

--- frontend/src/components/posts/PostList.js
+++ frontend/src/components/posts/PostList.js
@@ -1,45 +1,132 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext, useReducer } from 'react';
 import PostItem from './PostItem';
 import axios from 'axios';
-import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../UserContext';
+import Paginator from '../Paginator';
+import { Button } from 'react-bootstrap';
 
 
+// TODO
+// - [X] Add a paginator for the content
 
 
-const PostList = (props) => {
+const paginatorReducer = (state, action) => {
+	const getBounds = (page) => {
+		const low_bound = page*state.postsPerPage - (state.postsPerPage - 1);
+		const high_bound =  low_bound + state.postsPerPage - 1;
+		return {
+			low_bound: low_bound,
+			high_bound: high_bound
+		}
+	}
+	switch (action.type) {
+		case 'INCREMENT': {
+			const current_page = state.currentPage + 1;
+			const { low_bound, high_bound } = getBounds(current_page);
+			return {
+				...state,
+				currentPage: current_page,
+				low_bound: low_bound,
+				high_bound: high_bound
+			}
+		}
+		case 'DECREMENT': {
+			const current_page = state.currentPage - 1;
+			const { low_bound, high_bound } = getBounds(current_page);
+			return {
+				...state,
+				currentPage: current_page,
+				low_bound: low_bound,
+				high_bound: high_bound
+			}
+		}
+		case 'SET_PAGE_NUMBER': {
+			const low = action.payload*state.postsPerPage - state.postsPerPage + 1;
+			const high = low + state.postsPerPage - 1;
+			return {
+				...state,
+				currentPage: action.payload,
+				low_bound: low, 
+				high_bound: high
+			}
+		}
+		default: {
+			return state
+		}
+	}
+}
 
-	const [ posts, setPosts ] = useState([])
+
+const PostList = ({searchQuery}) => {
+
+	const [ state, dispatch ] = useContext(UserContext);
+	const [ posts, setPosts ] = useState([]);
+	const [ paginatorState, paginatorDispatch ] = useReducer(paginatorReducer, {
+		currentPage: 1,
+		postsPerPage: 3,
+		low_bound: 1,
+		high_bound: 3
+	});
 
 	useEffect(() => {
-		const fetchPosts = () => {
-			axios.get("http://127.0.0.1:8000/api/posts/")
-			.then(response => (
-				setPosts(response.data)
-			))
-			.catch(err => console.log(err))
+		const fetchPosts = async () => {
+			const response = await axios.get("http://127.0.0.1:8000/api/posts/");
+			if (response.status === 200) {
+				setPosts(response.data);
+			}
+			else {
+				console.log({"error": response.statusText});
+			}
 		}
-		fetchPosts();
-	}, []);
-
+		const fetchPost = (query) => {
+			console.log("Here with a query: ", query);
+			axios.get(
+				"http://127.0.0.1:8000/api/posts/query/", 
+				{ params:{"query": query} })
+			.then(response => setPosts(response.data))
+			.catch((err) => setPosts([]))
+		}
+		searchQuery? fetchPost(searchQuery) : fetchPosts();
+	}, [searchQuery]);
 
     return (
         <div>
-   	            {
-	            	posts.length>0? (
-	            		posts.map(post => (
-	            			<PostItem
-	  							key={post.id}
-	            				title={post.title}
-	            				content={post.content}
-	            				wordcount={post.word_count}
-	            				author={post.author}
-	            				tags={post.tags}
-	            			/>
-	            		))
-	            	) : (
-	            		<h3> Not posts yet@ </h3>
-	            	)
-	            }
+			{
+				posts.length>0? (
+					posts.slice(paginatorState.low_bound-1, paginatorState.high_bound).map(post => (
+						<PostItem
+							key={post.id}
+							id={post.id}
+							title={post.title}
+							content={post.content}
+							word_count={post.word_count}
+							author={post.author}
+							tags={post.tags}
+							likes_users={post.liked_by}
+						/>
+					))
+				) : (
+					<h3> Not posts yet@ </h3>
+				)
+			}
+			{ 
+				state.isAuthenticated? (
+					<Link to="/post/create">
+						<Button variant="primary"> Create Post </Button>
+					</Link>
+				) : ""
+			}
+			{
+				posts.length > paginatorState.postsPerPage? (
+					<Paginator 
+						postsPerPage={paginatorState.postsPerPage} 
+						totalPosts={posts.length} 
+						currentPage={paginatorState.currentPage} 
+						handlePageChange={paginatorDispatch}
+					/>
+				) : ""
+			}
         </div>
     );
 }
