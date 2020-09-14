163eef8b63f60a591370189b2228cc1db03f70f6
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-13 21:52:02 -0500

    The urls are now fixed

--- frontend/src/components/posts/PostItem.js
+++ frontend/src/components/posts/PostItem.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Card, Badge, Button } from 'react-bootstrap';
+import { Card, Badge, Button, Popover, OverlayTrigger } from 'react-bootstrap';
 import '../../assets/css/PostItem.css';
 import axios from 'axios';
+import { LinkContainer } from 'react-router-bootstrap';
 
 
+// TODO
+// - [ ] If user is authenticated enable the following buttons:
+// * Like
+// * Delete
+// * Edit (only if author of the post)
 
 
-const PostItem = ({id, title, content, word_count, author, tags}) => {
+const userPopover = (users) => {
+	return (
+		<Popover>
+			<Popover.Title as="h4"> Liked by </Popover.Title>
+			<Popover.Content>  
+				<ul>
+					<li> { users.map(user => <p key={user.id}> { user.username } </p> ) } </li>
+				</ul>
+			</Popover.Content>
+		</Popover>
+	)
+}
+
+
+const PostItem = ({id, title, content, word_count, author, tags, likes_users}) => {
     
     const deletePost = async (id) => {
         const response = await axios({
@@ -30,10 +50,24 @@ const PostItem = ({id, title, content, word_count, author, tags}) => {
 				</Card.Text>
 				<p className="post-card-content"> <strong className="post-card-content"> Words - { word_count } </strong> </p>
 				{
-					tags.length>0? tags.map(tag => <Badge key={ tag.id } variant="light">  { tag.title }  </Badge>) : ""
+					tags.length>0? tags.map(tag => <Badge key={ tag.id } variant="light" style={{ marginRight: "10px" }}>  { tag.title }  </Badge>) : ""
 				}
 				<br/>
-                                <Button variant="danger" onClick={() => deletePost(id)}> Delete </Button>
+				<div className="post-card-description">
+					{
+						likes_users.length > 0 ? (
+							<OverlayTrigger trigger="focus" placement="top" overlay={userPopover(likes_users)}>
+								<Button variant="success"> Likes <Badge variant="light"> { likes_users.length } </Badge> </Button>
+							</OverlayTrigger>
+						) : (
+							<Button variant="success"> Likes <Badge variant="light"> { likes_users.length } </Badge> </Button>
+						)
+					}
+					<Button variant="danger" onClick={() => deletePost(id)}> Delete </Button>
+					<LinkContainer to={`/posts/${id}`}> 
+						<Button variant="success"> Detail </Button>
+					</LinkContainer>
+				</div>
 			</Card.Body>
 		</Card>
     );
