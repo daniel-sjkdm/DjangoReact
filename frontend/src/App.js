64bee5627def010795f62408e232295a8b70e9d5
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-13 21:40:30 -0500

    The paths were changed properly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,39 +2,37 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import Navigation from './components/Navigation';
-import PostList from './components/posts/PostList';
+import Posts from './components/posts/Posts';
+import PostDetail from './components/posts/PostDetail';
 import PostForm from './components/posts/PostForm';
-import SideBar from './components/sidebar/SideBar';
 import RegisterForm from './components/register/RegisterForm';
 import LoginForm from './components/register/LoginForm';
+import PrivateRoute from './components/PrivateRoute';
+import { UserProvider } from './components/UserContext';
+import { loginReducer, initialState} from './reducers/loginReducer';
 import './assets/css/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 
 
 
 function App() {
   return (
     <div className="App" style={{ backgroundColor: "#242933"}}>
-      <Router>
-        <Navigation />
-        <Container fluid>
-          <Row>
-            <Col lg={3} md={8} xs={12} >
-              <SideBar />
-            </Col>
-            <Col lg={8} md={8} xs={12}>
-              <Switch>
-                <Route exact path="/" component={PostList} />
-                <Route path="/post/create" component={PostForm} />
-                <Route path="/accounts/register" component={RegisterForm} />
-                <Route path="/accounts/login" component={LoginForm} />
-              </Switch>
-            </Col>
-          </Row>
-          </Container>
-      </Router>
+      <UserProvider reducer={loginReducer} initialState={initialState}>
+      <Navigation />
+      <Container>
+        <Row>
+          <Col>
+            <Switch>
+              <Route exact path="/posts" component={Posts} />
+              <PrivateRoute exact path="/posts/create" component={PostForm}/>
+              <Route exact path="/posts/:id" component={PostDetail} />
+              <Route path="/accounts/register" component={RegisterForm} />
+              <Route path="/accounts/login" component={LoginForm} />
+            </Switch>
+          </Col>
+        </Row>
+        </Container>
+      </UserProvider>
     </div>
   );
 }
