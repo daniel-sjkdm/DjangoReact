1bf63543f3bfcfa247086e4c0c639c7b67fa6d1a
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-13 21:41:37 -0500

    Condionally render the user actions

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import '../assets/css/Navigation.css';
 
 
 const Navigation = (props) => {
+    const [ state, dispatch ] = useContext(UserContext);
     return (
         <Navbar expand="lg" className="navigation">
             <Navbar.Brand href="#"> Post App </Navbar.Brand>
             <Navbar.Toggle aria-controls="navigation-collapse"/>
             <Navbar.Collapse id="navigation-collapse">
                 <Nav className="ml-auto"> 
-                        <Link to="/">
-                            <Nav.Link className="navigation-link">
-                                Home
-                            </Nav.Link>
-                        </Link> 
-                        <NavDropdown title="User" id="navigation">
-                            <NavDropdown.Item> 
-                                <Link to="/register">
-                                    Register  
-                                </Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#"> 
-                                <Link to="/login">
-                                    Login 
-                                </Link>
-                            </NavDropdown.Item>
-                                <Link to="/logout">
-                                    <NavDropdown.Item href="#"> Logout </NavDropdown.Item>
-                                </Link>
+                        <NavDropdown title="USER" id="navigation" className="navigation-link">
+                            {
+                                state.isAuthenticated? (
+                                    <NavDropdown.Item> 
+                                        <LinkContainer to="/accounts/logout">
+                                            <p> Logout </p> 
+                                        </LinkContainer>
+                                    </NavDropdown.Item>
+                                ) : (
+                                    <React.Fragment>
+                                        <NavDropdown.Item> 
+                                            <LinkContainer to="/accounts/register">
+                                            <p> Register </p> 
+                                            </LinkContainer>
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item> 
+                                            <LinkContainer to="/accounts/login">
+                                                <p> Login </p> 
+                                            </LinkContainer>
+                                        </NavDropdown.Item>
+                                    </React.Fragment>
+                                )
+                            }
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
