ac2032f7eb3f6e69d80b49f9dcb09148b81fa281
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-13 21:45:14 -0500

    Authorization header included

--- frontend/src/components/posts/PostForm.js
+++ frontend/src/components/posts/PostForm.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import '../../assets/css/PostForm.css';
+import { authorizationHeader } from '../../services/auth';
 import axios from 'axios';
 
 
-const createPost = async (values) => {
-    const response = await axios({
-        url: "http://127.0.0.1:8000/api/posts/create/",
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        data: JSON.stringify(values)
-    })
-    response.status === 201 ? console.log(response.data) : console.log("Error")
-}
-
-
+    
 const PostForm = (props) => {
+
+    const createPost = async (values) => {
+        const authHeader = authorizationHeader();
+        console.log(authHeader)
+        const response = await axios({
+            url: "http://127.0.0.1:8000/api/posts/create/",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": authHeader.token 
+            },
+            data: JSON.stringify(values)
+        })
+        response.status === 201 ? console.log(response.data) : console.log("Error")
+    }
+    
+
+    const updatePost = async (values) => {
+        console.log(values)
+        const response = await axios({
+            url: `http://127.0.0.1:8000/api/posts/${props.id}/`,
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            data: JSON.stringify(values)
+        })
+        response.status === 201 ? console.log(response.data) : console.log("Error")
+    }
 
     const formik = useFormik({
         initialValues: {
-            title: "",
-            content: "",
-            tags: ""
+            title: props.title? props.title : "",
+            content: props.content? props.content : "",
+            tags: props.tags? props.tags.map(tag => tag.title) : ""
         },
         onSubmit: values => {
             createPost(values)
@@ -37,7 +55,8 @@ const PostForm = (props) => {
 
     return (
         <div className="root">
-            <Form onSubmit={formik.handleSubmit}> 
+            <Form onSubmit={props.id? ( () => updatePost(formik.values)) : formik.handleSubmit} autoComplete="false"> 
+            <p className="title"> Create a post! </p>
                 <Form.Group>
                     <Form.Label htmlFor="title"> Title </Form.Label>
                     <Form.Control 
@@ -56,13 +75,12 @@ const PostForm = (props) => {
                         value={formik.values.content}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        type="text"
+                        as="textarea"
+                        rows="8"
                         className="post-content"
                     />
-                    <div className="post-content-messages">
                         { formik.touched.content && formik.errors.content? <Form.Text className="error-message" style={{flexGrow: 1}}> { formik.errors.content } </Form.Text> : "" }
-                        { <Form.Text> { formik.values.content.length + "/" + 200 } </Form.Text> }
-                    </div>
+                        { <Form.Text className="helper-text"> { formik.values.content.length + "/" + 200 } </Form.Text> }
                 </Form.Group>
                 <Form.Group>
                     <Form.Label htmlFor="tags"> Tags </Form.Label>
@@ -73,9 +91,9 @@ const PostForm = (props) => {
                         onBlur={formik.handleBlur}
                         type="text"
                     />
-                    <Form.Text> Multiple tags must be comma separated </Form.Text>
+                    <Form.Text className="helper-text"> Multiple tags must be comma separated </Form.Text>
                 </Form.Group>
-                <Button type="submit" > Create </Button>
+                { props.id ? <Button type="submit"> Update </Button>  : <Button type="submit"> Submit </Button> }
             </Form>
         </div>
     );
